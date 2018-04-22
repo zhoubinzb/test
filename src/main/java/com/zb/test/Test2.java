7a1f8edb7708c37cb35f643b43e5a9950d6fbf45
zhoubinzb <zhoubinzb1@yahoo.com>
2018-04-22 21:50:34 +0800

    commit test

--- src/main/java/com/zb/test/Test2.java
+++ src/main/java/com/zb/test/Test2.java
@@ -12,7 +12,7 @@ private String a1;
 	public String getA1() {
 	return a1;
 }
-public void setA1(String a1) {
+	public void setA1(String a1) {
 	this.a1 = a1;
 }
 	public Date getDate() { return date;	}
